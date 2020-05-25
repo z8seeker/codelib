76b18ca682a4076a23599db51677c2bb4105465a
zhiqiangfang <zhiqiang.fang@guokr.com>
2020-05-25 11:28:12 +0800

    fix

--- write_csv.py
+++ write_csv.py
@@ -5,7 +5,7 @@ import os
 def write_csv(headlines, rows, filename):
     dir = os.path.dirname(os.path.realpath(__file__))
     fp = os.path.join(dir, filename)
-    with open(filename, 'w') as f:
+    with open(fp, 'w') as f:
         dw = DictWriter(f, fieldnames=headlines)
         writer.writeheader()
         for row in rows:
